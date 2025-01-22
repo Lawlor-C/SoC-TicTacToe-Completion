8a18751e26bd695be95133863f325f42990dea3b
Chris Lawlor <chris.d.lawlor@gmail.com>
2025-01-22 14:42:57 +0000

    added pseudo to app.jsx

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './App.css'
+
+// Grid - 3x3 - #1-9
+
+// 	3 rows of 3 buttons - default of null
+// Each button will have: Default state of Null
+// Returning the button nine times
+// Formatting these nine into 3x3 rows
+// Each square/button needs value of 1-9
+// Assign a state variable to each button so when clicked, change to X (for now)
+// Assign each button an index value 0-8
+
+
+// 	When clicked, changes to either X or O, alternating based on previous button’s state
+// If statement, tracking the current player's Token (X or O) and changing the state accordingly
+
+// Click only works on cells/buttons that are currently null
+
+
+
+// Null / X / O
+// Default Null
+
+
+// Draw the grid
+// Choose who goes first - Assign X
+// Change selected grid ref to X - P1
+// Change selected grid ref to O - P2
+// Until win
+
+
+
+
+
+
+
+// Winning conditions
+// 123
+// 456
+// 789
+// 147
+// 258
+// 369
+// 159
+// 357
